0713fac5c5662e36d66580e204306c77ebfc0cb7
Jack Liu Shurui <jackliusr@gmail.com>
2021-09-22 22:00:26 +0800

    Advanced C: Master and Demo Data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,14 +9,19 @@
     ],
     "Category": "estate",
     "data": [
+        "data/estate.property.type.csv",
         "security/security.xml",
-        "security/ir.model.access.csv",
+        "security/ir.model.access.csv",      
         "views/estate_property_offer_views.xml",
         "views/estate_property_tag_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_views.xml",
         "views/res_users_views.xml",
         "views/estate_menus.xml",
+    ],
+    "demo":[
+       "demo/property_demo.xml",
+       "demo/property_offer_demo.xml"  
     ],
     "application": True,
 }
